4b47e34419bffbebfe4eef30334260b05943086d
Lê Công Tuấn <congtuan14597>
2018-01-11 20:16:50 +0700

    sub_donmain

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#CT19"
+
+  namespace :admin do 
+    root "application#index"
+  end 
 end
+
+
+
+
+
+
+
+
+
+
